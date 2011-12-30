1ed28c4b311dfa3aa1325c877470004601952eb5
Kris Hemenway <krishemenway@gmail.com>
2011-12-30 00:10:30 -0800

    add restriction for handling multiple clicks quickly

--- book.js
+++ book.js
@@ -29,78 +29,82 @@
 			});
 			
 			function FlipLeft() {
-				var flip_page = $(".left-side").last();
-				var next_page = flip_page.prev(".page");
+				if($(".page_flipping").length == 0) {
+					var flip_page = $(".left-side").last();
+					var next_page = flip_page.prev(".page");
 
-				var zindexes = $(".right-side").map(function() { return parseInt($(this).css("z-index")); }).toArray();
-				var highest_index = (zindexes.length == 0) ? 0 : zindexes.max();
-				var first_z = ++highest_index;
-				var second_z = ++highest_index;
-				
-				flip_page.addClass("page_flipping");
-				next_page.addClass("page_flipping");
+					var zindexes = $(".right-side").map(function() { return parseInt($(this).css("z-index")); }).toArray();
+					var highest_index = (zindexes.length == 0) ? 0 : zindexes.max();
+					var first_z = ++highest_index;
+					var second_z = ++highest_index;
+					
+					flip_page.addClass("page_flipping");
+					next_page.addClass("page_flipping");
 
-				flip_page.animate({"width": 0, "left": params.page_width}, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-					$(this).css({"z-index": first_z, "width": 400}).addClass("right-side").removeClass("left-side");
-					$(this).removeClass("page_flipping");
-				}});
-				
-				next_page.css({"width": 0});
-				next_page.animate({"left": params.page_width / 2, "width": params.page_width / 2 }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-					$(this).css("z-index", 1000).addClass("right-side").removeClass("left-side");
-					
-					if($(".left-side").length == 0) {
-						left_arrow.hide();
-					} else {
-						left_arrow.show();
-					}
-					
-					right_arrow.show();
-					
-					$(this).animate({"left": params.page_width, "width": params.page_width }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-						$(".right-side").css("width", params.page_width);
-						$(this).css("z-index", second_z);
+					flip_page.animate({"width": 0, "left": params.page_width}, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+						$(this).css({"z-index": first_z, "width": 400}).addClass("right-side").removeClass("left-side");
 						$(this).removeClass("page_flipping");
 					}});
-				}});
+					
+					next_page.css({"width": 0});
+					next_page.animate({"left": params.page_width / 2, "width": params.page_width / 2 }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+						$(this).css("z-index", 1000).addClass("right-side").removeClass("left-side");
+						
+						if($(".left-side").length == 0) {
+							left_arrow.hide();
+						} else {
+							left_arrow.show();
+						}
+						
+						right_arrow.show();
+						
+						$(this).animate({"left": params.page_width, "width": params.page_width }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+							$(".right-side").css("width", params.page_width);
+							$(this).css("z-index", second_z);
+							$(this).removeClass("page_flipping");
+						}});
+					}});
+				}
 			}
 			
 			function FlipRight() {
-				var flip_page = $(".right-side").first();
-				var next_page = flip_page.next(".page");
-				next_page.css("left", params.page_width * 2);
+				if($(".page_flipping").length == 0) {
+					var flip_page = $(".right-side").first();
+					var next_page = flip_page.next(".page");
+					next_page.css("left", params.page_width * 2);
 
-				var indexes = $(".left-side").map(function() { return parseInt($(this).css("z-index")); }).toArray();
-				var highest_index = (indexes.length == 0) ? 0 : indexes.max();
-				var first_z = ++highest_index;
-				var second_z = ++highest_index;
-				
-				flip_page.addClass("page_flipping");
-				next_page.addClass("page_flipping");
+					var indexes = $(".left-side").map(function() { return parseInt($(this).css("z-index")); }).toArray();
+					var highest_index = (indexes.length == 0) ? 0 : indexes.max();
+					var first_z = ++highest_index;
+					var second_z = ++highest_index;
+					
+					flip_page.addClass("page_flipping");
+					next_page.addClass("page_flipping");
 
-				flip_page.animate({"width": 0}, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-					$(this).css({"left": 0, "z-index": first_z}).addClass("left-side").removeClass("right-side");
-					$(this).removeClass("page_flipping");
-				}});
-				
-				next_page.css({"width": 0});
-				next_page.animate({"left": params.page_width, "width": params.page_width / 2 }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-					$(this).css("z-index", 1000).addClass("left-side").removeClass("right-side");
-					
-					if($(".right-side").length == 0) {
-						right_arrow.hide();
-					} else {
-						right_arrow.show();
-					}
-					
-					left_arrow.show();
-					
-					$(this).animate({"left": 0, "width": params.page_width }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
-						$(".left-side").css("width", params.page_width);
-						$(this).css("z-index", second_z);
+					flip_page.animate({"width": 0}, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+						$(this).css({"left": 0, "z-index": first_z}).addClass("left-side").removeClass("right-side");
 						$(this).removeClass("page_flipping");
 					}});
-				}});
+					
+					next_page.css({"width": 0});
+					next_page.animate({"left": params.page_width, "width": params.page_width / 2 }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+						$(this).css("z-index", 1000).addClass("left-side").removeClass("right-side");
+						
+						if($(".right-side").length == 0) {
+							right_arrow.hide();
+						} else {
+							right_arrow.show();
+						}
+						
+						left_arrow.show();
+						
+						$(this).animate({"left": 0, "width": params.page_width }, { duration: params.turn_speed / 2, queue: false, easing: "linear", complete: function() {
+							$(".left-side").css("width", params.page_width);
+							$(this).css("z-index", second_z);
+							$(this).removeClass("page_flipping");
+						}});
+					}});
+				}
 			}
 			
 			$("body, .ui-dialog").keyup(function(event) {
